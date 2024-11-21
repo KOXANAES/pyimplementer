4708ae717d74be78a1fd233d37722963ad7e65b9
koxanaes <koxanaes@mail.ru>
2024-11-21 20:58:01 +0300

    Доделал базу

---
+++ execRequest.js
@@ -0,0 +1,18 @@
+document.getElementById('pyCodeForm').addEventListener('submit', (e) => {
+  event.preventDefault();
+  const code = document.getElementById('pyCodeForm__input').value;
+  fetch('http://localhost:5005/execute', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    body: new URLSearchParams({ code: code })
+  })
+  .then(response => response.json())
+  .then(data => {
+    const outputDiv = document.getElementById('pyCodeOutput');
+    if (data.error) {
+      outputDiv.innerHTML = `<strong>Ошибка:</strong> ${data.error}`;
+    } else {
+      outputDiv.innerHTML = `<strong>Результат:</strong> ${data.output}`;
+    }
+  });
+});
--- server.py
+++ server.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import sys
+import io
 
 app = Flask(__name__)
+CORS(app, resources={r"/execute": {"origins": "http://localhost:5500"}})
 
-@app.route("/")
-def hello_world():
-    return "<p>first python server (on flask!)</p>"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-app.run(host="0.0.0.0", port=5005, debug=True)
+@app.route('/execute', methods=['POST'])
+def execute_code():
+    code = request.form['code']
+    output = io.StringIO()
+    try:
+        sys.stdout = output
+        exec(code)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    finally:
+        sys.stdout = sys.__stdout__
+
+    return jsonify({'output': output.getvalue()})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5005, debug=True)
